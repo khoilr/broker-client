0afe78ca89604d48e8346089ac12be7d0b421037
khoilr <lcngmnhkhoi@gmail.com>
2023-06-01 14:38:59 +0700

    update

--- src/components/InputWhatsappUser.tsx
+++ src/components/InputWhatsappUser.tsx
@@ -3,13 +3,15 @@ import { Form, Input } from 'antd'
 export default function InputWhatsappUser() {
     return (
         <Form.Item
-            name='whatsapp-user'
-            label='Whatsapp user name'
+            name='whatsapp-number'
+            label='Whatsapp phone number'
             className='mx-2 w-full'
         >
             <Input
-                placeholder='@user'
+                placeholder='Whatsapp-number'
                 type='text'
+                minLength={10}
+                maxLength={10}
             />
         </Form.Item>
     )
