0d4aad4d975f9505c67016bf9b7ca488f76767a4
khoilr <lcngmnhkhoi@gmail.com>
2023-06-06 12:00:42 +0700

    change api domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function NotifyPage() {
     const onFinish = (values: any) => {
         console.log(values)
 
-        axios.post('https://colui.serveo.net/', values).then(response => {
+        axios.post('https://purpura.serveo.net/', values).then(response => {
             // print url and query params
             console.log(response.data)
         })
