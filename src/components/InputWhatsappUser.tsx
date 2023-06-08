7d2911666a097c91e71308a063f5009ae497d280
Ghienn <93633153+Ghienn@users.noreply.github.com>
2023-06-08 16:59:00 +0700

    Update InputWhatsappUser.tsx

--- src/components/InputWhatsappUser.tsx
+++ src/components/InputWhatsappUser.tsx
@@ -4,14 +4,14 @@ export default function InputWhatsappUser() {
     return (
         <Form.Item
             name='whatsapp-number'
-            label='Whatsapp phone number'
+            label='Whatsapp phone number (+84)'
             className='mx-2 w-full'
         >
             <Input
                 placeholder='Whatsapp-number'
                 type='text'
-                minLength={10}
-                maxLength={10}
+                minLength={11}
+                maxLength={11}
             />
         </Form.Item>
     )
