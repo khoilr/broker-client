fd9b045d86f34b72fd1ce2341c1a54c182a0e5cb
Ghienn <hienhine0310@gmail.com>
2023-07-10 10:27:01 +0700

    update UI

--- src/components/Login Form/SignUp.tsx
+++ src/components/Login Form/SignUp.tsx
@@ -62,7 +62,7 @@ export default function SignUp() {
                       <input
                           id='phonenumber'
                           name='phonenumber'
-                          type='number'
+                          type='text'
                           required
                           className='block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset sm:text-sm sm:leading-6'
                       />
